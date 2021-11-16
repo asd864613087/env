8dc1ed8f4f9395c22bb8941ce58fe1f3df0cf905
asd864613087 <sy1906915@buaa.edu.cn>
2021-11-16 20:48:04 +0800

    add ctx http

--- consts/consts.go
+++ consts/consts.go
@@ -1,15 +1,22 @@
 package consts
 
 const (
-	// Standard Name For Env
+	// Standard For Env Name
 	ENV_PSM       = "PSM"
 	ENV_NAMESPACE = "NAMESPACE"
 	ENV_PODNAME   = "HOSTNAME"
 
-	// Standard Name For Http Params
+	// Standard For Http Params Header Name
 	HEADER_SOURCE_PSM = "Paas-Source-Psm"
+	HEADER_PSM        = "Paas-Psm"
 	HEADER_DEST_PSM   = "Paas-Dest-Psm"
 	HEADER_LOG_ID     = "Paas-Log-Id"
 	HEADER_LOG_LEVEL  = "Paas-Log-Level"
-)
 
+	// Standard For Context Key Name
+	CTX_SOURCE_PSM = "source_psm"
+	CTX_PSM = "psm"
+	CTX_DEST_PSM = "dest_psm"
+	CTX_LOG_ID = "log_id"
+	CTX_LOG_LEVEL = "log_level"
+)
--- env.go
+++ env.go
@@ -1,7 +1,9 @@
 package env
 
 import (
+	"context"
 	"github.com/asd864613087/env/consts"
+	"net/http"
 	"os"
 )
 
@@ -15,4 +17,39 @@ func GetNamespace() string {
 
 func GetPodName() string {
 	return os.Getenv(consts.ENV_PODNAME)
+}
+
+func GetCtxSourcePsm(ctx context.Context) string {
+	v := ctx.Value(consts.CTX_SOURCE_PSM)
+	return v.(string)
+}
+
+func GetCtxPsm(ctx context.Context) string {
+	v := ctx.Value(consts.CTX_PSM)
+	return v.(string)
+}
+
+func SetHeaderSourcePsm(req *http.Request, sourcePsm string) *http.Request {
+	req.Header.Set(consts.HEADER_SOURCE_PSM, sourcePsm)
+	return req
+}
+
+func SetHeaderPsm(req *http.Request, Psm string) *http.Request {
+	req.Header.Set(consts.HEADER_PSM, Psm)
+	return req
+}
+
+func SetHeaderDestPsm(req *http.Request, destPsm string) *http.Request {
+	req.Header.Set(consts.HEADER_DEST_PSM, destPsm)
+	return req
+}
+
+func SetHeaderLogId(req *http.Request, logId string) *http.Request {
+	req.Header.Set(consts.HEADER_LOG_ID, logId)
+	return req
+}
+
+func SetHeaderLogLevel(req *http.Request, logLevel string) *http.Request {
+	req.Header.Set(consts.HEADER_LOG_LEVEL, logLevel)
+	return req
 }
